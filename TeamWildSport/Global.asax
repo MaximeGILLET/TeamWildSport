0b2c8a085ea029d8e9099c0a5fd747e99ab606dc
Maxime GILLET <gilet666@hotmail.fr>
2017-06-30 10:26:58 +0200

    Add project files.

---
+++ TeamWildSport/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamWildSport.MvcApplication" Language="C#" %>
---
+++ TeamWildSport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamWildSport.Startup))]
+namespace TeamWildSport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
